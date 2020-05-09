87a0ca7666a2e6c80274a27a7a9b84372737516d
Aiden Park <aiden.park@intel.com>
2020-05-08 23:49:17 -0700

    Update System Memory Base/Size from DeviceTree
    Signed-off-by: Aiden Park <aiden.park@intel.com>

--- Platform/ArmQemuBoardPkg/Library/Stage1BBoardInitLib/Stage1BBoardInitLib.c
+++ Platform/ArmQemuBoardPkg/Library/Stage1BBoardInitLib/Stage1BBoardInitLib.c
@@ -22,7 +22,9 @@
 #include <Library/BootloaderCoreLib.h>
 #include <Library/BoardSupportLib.h>
 #include <Library/ArmLib.h>
+#include <Library/FdtLib.h>
 
+#define DEVICE_TREE_BASE_ADDR       0x40000000
 #define MACH_VIRT_PERIPH_BASE       0x08000000
 #define MACH_VIRT_PERIPH_SIZE       SIZE_128MB
 
@@ -110,6 +112,75 @@ VirtualMemoryMapInit (
   (VOID)PcdSet64S (PcdVirtualMemoryMapTableBase, (UINT64)(UINTN)VirtualMemoryMapTable);
 }
 
+VOID
+PlatformMemoryInit (
+  VOID
+  )
+{
+  UINT64        SystemMemoryBase;
+  UINT64        SystemMemorySize;
+  INT32         MemoryNode;
+  INT32         AddressCells;
+  INT32         SizeCells;
+  INT32         Length;
+  VOID          *DeviceTreeBlob;
+  CONST INT32   *Prop;
+
+  DeviceTreeBlob = (VOID *)(UINTN)DEVICE_TREE_BASE_ADDR;
+  if (fdt_check_header (DeviceTreeBlob) != 0) {
+    return;
+  }
+
+  //
+  // Look for a node called "memory" at the lowest level of the tree
+  //
+  MemoryNode = fdt_path_offset (DeviceTreeBlob, "/memory");
+  if (MemoryNode <= 0) {
+    return;
+  }
+
+  //
+  // Retrieve the #address-cells and #size-cells properties
+  // from the root node, or use the default if not provided.
+  //
+  AddressCells = 1;
+  SizeCells = 1;
+
+  Prop = fdt_getprop (DeviceTreeBlob, 0, "#address-cells", &Length);
+  if (Length == 4) {
+    AddressCells = fdt32_to_cpu (*Prop);
+  }
+
+  Prop = fdt_getprop (DeviceTreeBlob, 0, "#size-cells", &Length);
+  if (Length == 4) {
+    SizeCells = fdt32_to_cpu (*Prop);
+  }
+
+  //
+  // Now find the 'reg' property of the /memory node, and read the first
+  // range listed.
+  //
+  Prop = fdt_getprop (DeviceTreeBlob, MemoryNode, "reg", &Length);
+
+  if (Length < (AddressCells + SizeCells) * sizeof (INT32)) {
+    return;
+  }
+
+  SystemMemoryBase = fdt32_to_cpu (Prop[0]);
+  if (AddressCells > 1) {
+    SystemMemoryBase = (SystemMemoryBase << 32) | fdt32_to_cpu (Prop[1]);
+  }
+  Prop += AddressCells;
+
+  SystemMemorySize = fdt32_to_cpu (Prop[0]);
+  if (SizeCells > 1) {
+    SystemMemorySize = (SystemMemorySize << 32) | fdt32_to_cpu (Prop[1]);
+  }
+
+  (VOID)PcdSet64S (PcdSystemMemoryBase, SystemMemoryBase);
+  (VOID)PcdSet64S (PcdSystemMemorySize, SystemMemorySize);
+}
+
 /**
   Board specific hook points.
 
@@ -132,11 +203,9 @@ BoardInit (
     PlatformNameInit ();
     break;
   case PreMemoryInit:
+    // Use PreMemoryInit for now
+    PlatformMemoryInit ();
     VirtualMemoryMapInit ();
-    break;
-  case PostMemoryInit:
-  case PreTempRamExit:
-  case PostTempRamExit:
     break;
   default:
     break;
